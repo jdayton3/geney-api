152de4e03fad6e3bfbfd13c9d2bbb3bafb77d411
Jonathan Dayton <j.dayton.byu@gmail.com>
2017-06-12 22:45:44 -0600

    Start handling meta data and datasets

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/api/datasets')
+def datasets():
+    return "There are no datasets."
+
+@app.route('/api/meta/<string:dataset_id>', methods=['GET'])
+def meta(dataset_id):
+    return "You input a dataset id: %s" % dataset_id
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test/test_app.py
+++ test/test_app.py
@@ -9,15 +9,36 @@ import json
 from app import *
 
 
-class TestRoutesWork(unittest.TestCase):
+class RouteTester(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
 
+class TestRoutesWork(RouteTester):
     def test_IndexDoesNotExist(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 404)
 
-    def test_Datasets(self):
+    def test_404ReturnsJSON(self):
+        response = self.app.get('/nothing/here')
+        expected = {'error': 'Not found'}
+        actual = json.loads(response.data)
+        self.assertEqual(actual, expected)
+
+
+class TestDatasets(RouteTester):
+    def test_DatasetsRouteExists(self):
         response = self.app.get('/api/datasets')
+        self.assertEqual(response.status_code, 200)
+
+    def test_ResponseIsJsonObject(self):
+        response = self.app.get('/api/datasets')
+        try:
+            expected = json.loads(response.data)
+        except ValueError as e:
+            self.fail("Failed to load JSON (%s). %s" % (response.data, e))
+
+class TestMeta(RouteTester):
+    def test_MetaRouteExists(self):
+        response = self.app.get('/api/meta/sampledataset')
         self.assertEqual(response.status_code, 200)
