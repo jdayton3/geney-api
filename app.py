1386e4919d4ee892269a5f2eda59cdb2fc7dee6e
Jonathan Dayton <j.dayton.byu@gmail.com>
2017-06-14 12:54:51 -0600

    Migrate Flask API to a class

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 #!flask/bin/python
 from flask import Flask, make_response, jsonify
 
-app = Flask(__name__)
+# TODO: rename this class
+class MyAPI:
+    def __init__(self):
+        self.app = Flask(__name__)
+        thing = []
+        self.app.add_url_rule('/api/datasets', 'datasets', self.datasets)
+        self.app.add_url_rule('/api/meta/<string:dataset_id>', 'meta', self.meta)
+        self.app.register_error_handler(404, self.not_found)
+        #self.app.errorhandler(404)
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    #@app.errorhandler(404)
+    def not_found(self, error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
 
-@app.route('/api/datasets')
-def datasets():
-    return "There are no datasets."
+    #@app.route('/api/datasets')
+    def datasets(self):
+        return jsonify([])
 
-@app.route('/api/meta/<string:dataset_id>', methods=['GET'])
-def meta(dataset_id):
-    return "You input a dataset id: %s" % dataset_id
+    #@app.route('/api/meta/<string:dataset_id>', methods=['GET'])
+    def meta(self, dataset_id):
+        return jsonify({"thing": "You input a dataset id: %s" % dataset_id})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    api = MyAPI()
+    api.app.run(debug=True)
--- test/test_app.py
+++ test/test_app.py
@@ -11,8 +11,9 @@ from app import *
 
 class RouteTester(unittest.TestCase):
     def setUp(self):
-        app.testing = True
-        self.app = app.test_client()
+        api = MyAPI()
+        api.app.testing = True
+        self.app = api.app.test_client()
 
 class TestRoutesWork(RouteTester):
     def test_IndexDoesNotExist(self):
@@ -31,14 +32,28 @@ class TestDatasets(RouteTester):
         response = self.app.get('/api/datasets')
         self.assertEqual(response.status_code, 200)
 
-    def test_ResponseIsJsonObject(self):
+    def test_ResponseIsJsonArray(self):
         response = self.app.get('/api/datasets')
         try:
             expected = json.loads(response.data)
         except ValueError as e:
             self.fail("Failed to load JSON (%s). %s" % (response.data, e))
+        self.assertEqual(list, type(expected))
 
 class TestMeta(RouteTester):
     def test_MetaRouteExists(self):
         response = self.app.get('/api/meta/sampledataset')
         self.assertEqual(response.status_code, 200)
+
+
+    def test_ResponseIsJsonObj(self):
+        response = self.app.get('/api/meta/sampledataset')
+        try:
+            expected = json.loads(response.data)
+        except ValueError as e:
+            self.fail("Failed to load JSON (%s). %s" % (response.data, e))
+        self.assertEqual(dict, type(expected))
+
+    def test_NextSteps(self):
+        message = "I need to include a DBAccess object in the __init__ function of MyAPI"
+        self.fail(message)
